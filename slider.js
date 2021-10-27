c2cfe981ed4060652a507ee52de2229216346912
xDunkan <xdunkanx@mail.ru>
2021-10-27 10:57:49 +0600

    slider new

--- slider.js
+++ slider.js
@@ -1,127 +1,88 @@
-const slides = document.querySelectorAll('.slider__item');
-const slider = [];
-const prev = document.querySelector('.slider__prev');
+// "use strict";
+
+const track = document.querySelector('.slider__track');
 const next = document.querySelector('.slider__next');
-
-for (let i = 0; i < slides.length; i++) {
-	dotsAdd();
-}
-
-function dotsAdd() {
-	let dot = document.createElement('div');
-	dot.classList.add('slider__dot');
-	dot.classList.add('dot-active');
-	document.querySelector('.slider__dots').appendChild(dot);
-}
-
+const prev = document.querySelector('.slider__prev');
 const dots = document.querySelectorAll('.slider__dot');
 
-for (let i = 0; i < slides.length; i++) {
-	slider[i] = slides[i].src;
-	slides[i].remove();
-}
 
-let step = 0;
-let offset = 0;
-let count = 0;
+let position = -300;
+let timer;
+let currentDot;
+let dot = Array.from(dots);
 
+track.style.transform = `translateX(${position}px)`;
 
-function draw() {
-	let img = document.createElement('img');
-	img.src = slider[step];
-	img.classList.add('slider__item');
-	img.style.left = offset * 300 + 'px';
-	document.querySelector('.slider__items').appendChild(img);
-	if (step + 1 == slider.length) {
-		step = 0;
-	} else {
-		step++;
-	}
-	offset = 1;
-	console.log('step ' + step);
-	console.log('offset ' + offset);
-}
-
-draw(); draw();
-
-function left() {
-	next.onclick = null;
-	let slides2 = document.querySelectorAll('.slider__item');
-	let offset2 = 0;
-	for (let i = 0; i < slides2.length; i++) {
-		slides2[i].style.left = offset2 * 300 - 300 + 'px';
-		offset2++;
-	}
-	setTimeout(function () {
-		slides2[0].remove();
-		draw();
-		next.onclick = left;
-	}, 300);
-}
-
-
-// function draw2() {
-// 	let img3 = document.createElement('img');
-// 	img3.src = slider[step - 1];
-// 	img3.classList.add('slider__item');
-// 	img3.style.left = -offset * 300 + 'px';
-// 	document.querySelector('.slider__items').appendChild(img3);
-// 	if (step - 1 == -1) {
-// 		step = slider.length - 1;
-// 	} else {
-// 		step--;
-// 	}
-// 	offset = 1;
-// 	console.log('step ' + step);
-// 	console.log('offset ' + offset);
-// };
-// draw2();
-
-// function right() {
-// 	prev.onclick = null;
-// 	let slides2 = document.querySelectorAll('.slider__item');
-// 	let offset2 = 0;
-// 	for (let i = 0; i < slides2.length; i++) {
-// 		slides2[0].style.left = 0;
-// 		offset2++;
-// 	}
-// 	setTimeout(function () {
-// 		slides2[0].remove();
-// 		draw2();
-// 		prev.onclick = right;
-// 	}, 300);
-// }
-
-
-next.addEventListener('click', left);
 next.addEventListener('click', () => {
-	if (count + 1 > slides.length - 1) { count = -1 };
-	count++;
-	dotActive();
+	timer = setInterval(slideNext, 4);
+	console.log(`currentDot: ${currentDot}`);
+	console.log(`position: ${position}`);
 });
 
-
-// prev.addEventListener('click', right);
 prev.addEventListener('click', () => {
-	if (count - 1 < 0) { count = 10 };
-	count--;
-	dotActive();
+	timer = setInterval(slidePrev, 4);
+	console.log(`currentDot: ${currentDot}`);
+	console.log(`position: ${position}`);
 });
 
-function dotActive() {
-	for (let i = 0; i < dots.length; i++) {
-		dots[i].classList.remove('dot-active');
-		dots[count].classList.add('dot-active');
+function slideNext() {
+	next.disabled = true;
+	position -= 5;
+	track.style.transform = `translateX(${position}px)`;
+	if (position % 300 === 0) {
+		clearInterval(timer);
+		dotsShifting();
+		next.disabled = false;
+	}
+	if (position < -2700) {
+		position = 0;
 	}
 };
-dotActive();
 
+function slidePrev() {
+	prev.disabled = true;
+	position += 5;
+	track.style.transform = `translateX(${position}px)`;
+	if (position % 300 === 0) {
+		clearInterval(timer);
+		dotsShifting();
+		prev.disabled = false;
+	}
+	if (position > -300) {
+		position = -3000;
+	}
+};
 
+function dotsShifting() {
+	currentDot = Math.abs(position / 300);
+	dots.forEach((item, i) => {
+		if ((i + 1) === currentDot) {
+			item.classList.add('dot-active');
+		} else {
+			item.classList.remove('dot-active');
+		}
+	});
+	return currentDot;
+};
 
+dotsShifting();
 
+dot.forEach((item, i) => {
+	item.addEventListener('click', () => {
+		currentDot = i + 1;
+		position = -300 * currentDot;
+		track.style.transform = `translateX(${position}px)`;
 
-
-
-
-
-
+		dots.forEach((item, i) => {
+			if ((i + 1) === currentDot) {
+				item.classList.add('dot-active');
+			} else {
+				item.classList.remove('dot-active');
+			}
+		});
+		console.log(`currentDot: ${currentDot}`);
+		console.log(`position: ${position}`);
+	})
+	return currentDot;
+});
+console.log(dot);
